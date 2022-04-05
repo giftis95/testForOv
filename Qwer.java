bc9fef2351ad8a44107f2bd1c6d5a5ed8ac75717
giftis95 <giftis95@mail.ru>
2022-04-05 10:07:22 +0300

    Signed-off-by: giftis95 <giftis95@mail.ru> fixed bags

--- Qwer.java
+++ Qwer.java
@@ -1,5 +1,6 @@
 public class Qwer{
     public static void main(String[] args) {
-        System.out.println("Hello world!"+a);
+        System.out.println("Hello world!");
+	System.out.println("Bye world");
     }
 }
